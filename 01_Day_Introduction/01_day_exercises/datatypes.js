f427bb715eb350f2fe5f16c457c42ad1133fa358
Sylvia Mumbi <mumbisylvia.m@gmail.com>
2026-01-14 09:19:10 +0300

    day 1 Q5 complete

---
+++ 01_Day_Introduction/01_day_exercises/datatypes.js
@@ -0,0 +1,23 @@
+let secondName = 'Mumbi'
+console.log((typeof secondName))
+
+let myAge = 25
+console.log(typeof myAge)
+
+let job
+console.log(typeof job)
+
+let count = null
+console.log(typeof count)
+
+let books = ['Silent Patient', 'The Seven Husbands of Evelyn Hugo', 'The Mountain Is You']
+console.log(typeof books)
+
+let has_dog = false
+console.log(typeof has_dog)
+
+let greet = function () {
+  return "Hi, I'm Sylvia";
+};
+console.log(typeof greet);
+
